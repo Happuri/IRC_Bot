9f0a791923de3ae96e98b2e0583195809bfaba8b
Happuri <happuri@gmx.com>
2014-07-14 11:54:07 +0200

    Display configuration

---
+++ display-conf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+source conf/irc.conf 
+
+echo "Output from chat is in file: $FROM" 
+echo "Input to chat is in file: $TO" 
+echo "Bot nick: $NICK"
+echo "You can run bot with command: Debug/IRC_Bot $NICK $FROM $TO"
